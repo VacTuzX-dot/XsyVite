3e9ad63f65079077d1ae0866fdd6593d6151fa80
Gamemine098 <tanagameggth@gmail.com>
2024-10-01 18:36:56 +0700

    Update Homepage

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,8 @@
 /** @type {import('prettier').Config} */
-module.exports = {
+const config = {
   plugins: ["prettier-plugin-tailwindcss"],
-  // tailwindcss
   tailwindAttributes: ["theme"],
   tailwindFunctions: ["twMerge", "createTheme"],
 };
+
+export default config;
