ead520a5de52d1ee0f62c138eba2d940c8fe9893
Gamemine098 <tanagameggth@gmail.com>
2024-10-27 20:53:16 +0700

    Update About

--- src/pages/about_nice.tsx
+++ src/pages/about_nice.tsx
@@ -4,7 +4,7 @@ import "../style/index.css";
 import About from "./about";
 // Import other things that require
 import React from "react";
-import { Breadcrumb, BreadcrumbItem } from "flowbite-react";
+import { Breadcrumb, BreadcrumbItem, Avatar, Tabs } from "flowbite-react";
 import { Link, Route, Routes } from "react-router-dom";
 
 const About_Nice: React.FC = () => {
@@ -38,7 +38,58 @@ const About_Nice: React.FC = () => {
 
         {/* Content */}
         <div className="animate__animated animate__fadeInDown animate__faster mx-6 mt-5 rounded-xl bg-slate-400 bg-opacity-40 pb-7 pt-8 text-center drop-shadow-xl">
-          <h1 className="text-2xl drop-shadow-md">About Me!</h1>
+          <h1 className="pb-9 text-2xl drop-shadow-md">About Me!</h1>
+          <div className="flex flex-wrap items-center justify-center gap-4">
+            <Avatar size="xl" alt="placeholder" />
+            <p>Test</p>
+          </div>
+          <div className="mx-36 mt-10 overflow-x-auto">
+            <Tabs
+              aria-label="Full width tabs"
+              variant="fullWidth"
+              className="dark"
+            >
+              <Tabs.Item active title="Profile">
+                This is{" "}
+                <span className="font-medium text-gray-800">
+                  Profile tabs associated content
+                </span>
+                . Clicking another tab will toggle the visibility of this one
+                for the next. The tab JavaScript swaps classes to control the
+                content visibility and styling.
+              </Tabs.Item>
+              <Tabs.Item title="Dashboard">
+                This is{" "}
+                <span className="font-medium text-gray-800">
+                  Dashboard tabs associated content
+                </span>
+                . Clicking another tab will toggle the visibility of this one
+                for the next. The tab JavaScript swaps classes to control the
+                content visibility and styling.
+              </Tabs.Item>
+              <Tabs.Item title="Settings">
+                This is{" "}
+                <span className="font-medium text-gray-800">
+                  Settings tabs associated content
+                </span>
+                . Clicking another tab will toggle the visibility of this one
+                for the next. The tab JavaScript swaps classes to control the
+                content visibility and styling.
+              </Tabs.Item>
+              <Tabs.Item title="Contacts">
+                This is{" "}
+                <span className="font-medium text-gray-800">
+                  Contacts tabs associated content
+                </span>
+                . Clicking another tab will toggle the visibility of this one
+                for the next. The tab JavaScript swaps classes to control the
+                content visibility and styling.
+              </Tabs.Item>
+              <Tabs.Item disabled title="Disabled">
+                Disabled content
+              </Tabs.Item>
+            </Tabs>
+          </div>
         </div>
       </div>
 
